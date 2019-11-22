1331b6654ce29e0d488f6df50693c1832baaba78
DiogoASTeixeira <diogo_alex9@hotmail.com>
2019-11-22 14:40:41 +0000

    Complete Purchase Order Form

--- src/components/PurchaseOrderForm.js
+++ src/components/PurchaseOrderForm.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import LabelInput from './LabelInput';
+import React, { Component } from 'react';
+import LabelTextInput from './LabelTextInput';
+import LabelNumberInput from './LabelNumberInput';
 
 export default class PurchaseOrderForm extends Component {
     constructor(props) {
@@ -9,12 +10,12 @@ export default class PurchaseOrderForm extends Component {
             docType: '',
             serial: '',
             number: '',
-            date: '',
+            date: this.getCurrentDate(),
             reference: '',
             supplier: '',
             paymentCondition: '',
             supplyCondition: '',
-            discount: ''
+            discount: '0.00'
         }
 
         this.state = this.initialState;
@@ -22,36 +23,88 @@ export default class PurchaseOrderForm extends Component {
 
 
     render() {
-        const { docType, serial, number } = this.state;
+        const { docType, serial, number, date, reference, supplier, paymentCondition, supplyCondition, discount } = this.state;
 
         return (
             <form>
-                <LabelInput
+                <LabelTextInput
                     label="Tipo de Documento"
                     name="docType"
                     value={docType}
                     onChange={this.handleChange}
                 />
-                <LabelInput
+                <LabelNumberInput
                     label="Série"
+                    format="#### #### #### ####"
                     name="serial"
                     value={serial}
                     onChange={this.handleChange}
                 />
-                <LabelInput
+                <LabelNumberInput
                     label="Número"
                     name="number"
                     value={number}
                     onChange={this.handleChange}
                 />
+                <LabelNumberInput
+                    label="Data"
+                    format="##/##/####"
+                    placeholder="DD/MM/YYYY"
+                    mask={['D', 'D', 'M', 'M', 'Y', 'Y', 'Y', 'Y']}
+                    name="date"
+                    value={date}
+                    onChange={this.handleChange}
+                />
+                <LabelNumberInput
+                    label="Referência"
+                    name="reference"
+                    value={reference}
+                    onChange={this.handleChange}
+                />
+
+                <LabelTextInput
+                    label="Fornecedor"
+                    name="supplier"
+                    value={supplier}
+                    onChange={this.handleChange}
+                />
+                <LabelTextInput
+                    label="Condição de Pagamento"
+                    name="paymentCondition"
+                    value={paymentCondition}
+                    onChange={this.handleChange}
+                />
+                <LabelTextInput
+                    label="Condição de Envio"
+                    name="supplyCondition"
+                    value={supplyCondition}
+                    onChange={this.handleChange}
+                />
+                <LabelNumberInput
+                    label="Desconto"
+                    suffix="%"
+                    decimalScale="2"
+                    fixedDecimalScale="true"
+                    name="discount"
+                    value={discount}
+                    onChange={this.handleDiscountChange}
+                />
                 <input type="button" value="Submit" onClick={this.submitForm} />
             </form>
-        )
+        );
+    }
+
+    handleDiscountChange = event => {
+        const { name, value } = event.target;
+        let parsedVal = parseFloat(value);
+        this.setState({
+            [name]: this.discountLimit(parsedVal)
+        });
+
     }
 
     handleChange = event => {
         const { name, value } = event.target;
-
         this.setState({
             [name]: value
         });
@@ -60,5 +113,24 @@ export default class PurchaseOrderForm extends Component {
     submitForm = () => {
         this.props.handleSubmit(this.state);
         this.setState(this.initialState);
+    }
+
+    getCurrentDate(separator = '') {
+        let newDate = new Date();
+        let day = newDate.getDate();
+        let month = newDate.getMonth();
+        let year = newDate.getFullYear();
+        console.log(`${day}${month < 10 ? `0${month}` : `${month}`}${year}`);
+        return `${day}${month < 10 ? `0${month}` : `${month}`}${year}`;
+    }
+
+    discountLimit(val) {
+        return this.minMaxNumberLimit(val, 0.00, 100.00);
+    }
+
+    minMaxNumberLimit(val, min, max) {
+        if (val < min) return `${min}`;
+        if (val > max) return `${max}`;
+        else return `${val}`;
     }
 }
