1742f6ad80bb732d0b74c086fdd6356efb6d1ad2
DiogoASTeixeira <diogo_alex9@hotmail.com>
2019-12-16 11:25:00 +0000

    Dashboard stuff, am tired

--- Frontend-Supplier/src/components/Dashboard/Dashboard.js
+++ Frontend-Supplier/src/components/Dashboard/Dashboard.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import Helmet from "react-helmet";
+import BarGraph from './BarGraph';
+import ProductAmountProfit from './ProductAmountProfit';
+import { Container } from '@material-ui/core';
+import axios from 'axios';
 
 export default class Dashboard extends Component {
     render() {
         return (
-            <div>
+            <Container style={{ 'minWidth': '1000px' }}>
                 <Helmet>
                     <meta charset="utf-12" />
                     <title>DashBoard - Supplier</title>
                 </Helmet>
-                DashBoard
-                
-            </div>
+                <BarGraph title={'Monthly Orders in Current Year'} />
+                <ProductAmountProfit />
+            </Container>
         )
+    }
+
+    componentWillMount = () => {
+        axios.get(`http://localhost:8000/sinfApi/login`);
     }
 }
